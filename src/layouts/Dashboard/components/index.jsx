db1714585fdb19abdf9fdad8816c582a37e42a0a
Kratharth <kratharthhegde.cs17@bmsce.ac.in>
2019-07-05 16:58:40 +0530

    Default project

---
+++ src/layouts/Dashboard/components/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+class Type extends React.Component {
+    render() {
+      return <p>''./Sidebar/SidebarStudent'</p>;
+    }
+  }
+
+export { default as Footer } from './Footer';
+//export { default as Sidebar } from './Sidebar/SidebarStudent'
+export { default as Topbar } from './Topbar';
+
+
+ // get the React object from the react module
+
+
+
+export default Type; // expose the HelloWorld component to other modules
