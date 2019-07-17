f21e758944904ef024e8dceced144e608798305c
Kratharth <kratharthhegde.cs17@bmsce.ac.in>
2019-07-17 14:17:38 +0530

    merged with test and sign in API

---
+++ src/views/ProductListedit/components/index.jsx
@@ -0,0 +1,2 @@
+//export { default as ProductCard } from '../../.././components/ProductCard';
+export { default as ProductsToolbar } from './ProductsToolbar';
---
+++ src/views/Videorecordguest/components/VideocontrolDetails/styles.jsx
@@ -0,0 +1,69 @@
+
+import { green } from '@material-ui/core/colors';
+import { Button } from '@material-ui/core';
+
+export default theme => ({
+  formControl: {
+    //  margin: theme.spacing(3),
+    },
+    group: {
+      margin: theme.spacing(1, 0),
+    },
+
+  field: {
+    margin: theme.spacing.unit * 3
+  },
+  textField: {
+    width: '420px',
+    maxWidth: '100%',
+    marginRight: theme.spacing.unit * 3
+  },
+  portletFooter: {
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  },
+  palette: {
+    primary: green,
+  },
+  margin: {
+      margin: theme.spacing(1),
+    },
+
+    root: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    //backgroundColor: '#007bff',
+    //borderColor: '#007bff',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+    //  backgroundColor: '#0069d9',
+      //borderColor: '#0062cc',
+    },
+    '&:active': {
+      boxShadow: 'none',
+    //  backgroundColor: '#0062cc',
+    //  borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+
+});
